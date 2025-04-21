5da8d5ea66eb0a8a271905095df9be9c8fdde2ab
musthave-prog <darya.sim.ds@gmail.com>
2025-04-21 13:18:30 +0300

    пробел

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,10 +1,8 @@
 from django.urls import include, path
 
-from django.urls import path, include
 
 urlpatterns = [
     path('v1/', include('titles.urls')),
     path('v1/', include('users.urls')),
-  path('v1/', include('reviews.urls')),
+    path('v1/', include('reviews.urls')),
 ]
-
