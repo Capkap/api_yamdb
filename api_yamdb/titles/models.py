7ead7f0ddee9e2cd69a1edad79d8e006c5a30b53
musthave-prog <darya.sim.ds@gmail.com>
2025-04-21 12:06:21 +0300

    Update models.py

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -1,9 +1,1 @@
-from django.db import models
 
-class Title(models.Model):
-    rating = models.FloatField(null=True, blank=True)
-
-    def update_average_rating(self):
-        from django.db.models import Avg
-        self.rating = self.reviews.aggregate(Avg('score'))['score__avg']
-        self.save(update_fields=['rating'])
