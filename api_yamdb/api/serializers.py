671cdccc35719d54d1760f339cccf372709941ea
musthave-prog <darya.sim.ds@gmail.com>
2025-04-22 17:48:21 +0300

    Fix Meta in model Comment

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -55,8 +55,7 @@ class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
-        fields = '__all__'
-        read_only_fields = ('review',)
+        exclude = ('review',)
 
     def validate(self, attrs):
         if self.context['request'].method == 'POST':
