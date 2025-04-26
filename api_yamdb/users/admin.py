a05c19bcf0ec4be6d975c794f05a2da8a03f3431
Yoishiii <shporkz@gmail.com>
2025-04-27 00:47:59 +0300

    added admin file for users

---
+++ api_yamdb/users/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from users.models import User
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'role', 'is_staff', 'is_superuser')
+    list_filter = ('role', 'is_staff', 'is_superuser')
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Персональная информация', {'fields':
+                                     ('first_name', 'last_name', 'bio')}),
+        (
+            'Права доступа',
+            {'fields': (
+                'role', 'is_active', 'is_staff',
+                'is_superuser', 'groups', 'user_permissions')}
+        ),
+        ('Важные даты', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'username', 'email', 'password1', 'password2',
+                'role', 'is_staff', 'is_superuser'
+            ),
+        }),
+    )
