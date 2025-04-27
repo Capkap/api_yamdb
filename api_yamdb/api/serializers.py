f8416a89880e87b66d090a74ec1d7087b31285fa
musthave-prog <darya.sim.ds@gmail.com>
2025-04-27 12:32:49 +0300

    admin reviews, readme

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -2,7 +2,7 @@ from django.core.validators import RegexValidator
 from rest_framework import serializers
 
 from api.validators import validate_score_range, validate_year
-from api_yamdb import constants
+from api_yamdb.constants import DEFAULT_RATING_VALUE
 from reviews.models import Comment, Review
 from titles.models import Category, Genre, Title
 from users.models import User
@@ -134,7 +134,7 @@ class TitleGETSerializer(serializers.ModelSerializer):
 
     genre = GenreSerializer(many=True)
     category = CategorySerializer()
-    rating = serializers.IntegerField(default=constants.DEFAULT_RATING_VALUE)
+    rating = serializers.IntegerField(default=DEFAULT_RATING_VALUE)
 
     class Meta:
         model = Title
